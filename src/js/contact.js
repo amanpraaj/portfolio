0b151cb8ebef89b3b3a78f86b793269d9575f312
Aman Prajapat <109981086+amanpraaj@users.noreply.github.com>
2023-10-30 22:47:41 +0530

    Create contact.js

---
+++ src/js/contact.js
@@ -0,0 +1,20 @@
+const url = 'php/contact.php';
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const formData = new FormData(document.getElementById("contactForm"));
+
+    fetch(url, {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("contactForm").reset();
+        document.getElementById("confirmation").style.display = "block";
+        document.getElementById("confirmation").innerHTML = data;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
