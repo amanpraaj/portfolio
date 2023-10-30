710f228ab2599135760094b84ef2b48fdb830322
Aman Prajapat <109981086+amanpraaj@users.noreply.github.com>
2023-10-30 23:14:28 +0530

    Update contact.js

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,20 +1,27 @@
-const url = 'https://about.amanpraaj.eu.org/src/php/contact_form.php';
+<script>
+    emailjs.init("PlUuFaRIStZqbosy3");
 
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    const formData = new FormData(document.getElementById("contactForm"));
+    document.getElementById("contactForm").addEventListener("submit", function(event) {
+        event.preventDefault();
 
-    fetch(url, {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("contactForm").reset();
-        document.getElementById("confirmation").style.display = "block";
-        document.getElementById("confirmation").innerHTML = data;
-    })
-    .catch(error => {
-        console.error('Error:', error);
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const message = document.getElementById("message").value;
+
+        const templateParams = {
+            from_name: name,
+            from_email: email,
+            message: message,
+        };
+
+        emailjs.send("service_da0974b", "PlUuFaRIStZqbosy3", templateParams)
+            .then(function(response) {
+                console.log("Email sent:", response);
+                alert("Message sent successfully!");
+                document.getElementById("contactForm").reset();
+            }, function(error) {
+                console.error("Email not sent:", error);
+                alert("Message not sent. Please try again later.");
+            });
     });
-});
+</script>
